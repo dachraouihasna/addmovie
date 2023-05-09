6c1070f99ae1bc14502bd545de9081c206ee4b9b
hasnadachraoui <hdachraoui743@gmail.com>
2023-05-09 22:26:24 +0100

    add farah

---
+++ src/Components/MoviesData.js
@@ -0,0 +1,57 @@
+export const moviesData = [
+  {
+    id: Math.random(),
+    rating: 4,
+    name: "Devil's Mile",
+    date: "2014",
+    type: "Action | Crime",
+    description:
+      "A relentlessly-paced hybrid of gritty crime thriller and Lovecraftian supernatural horror, The Devil's Mile follows a trio of kidnappers who take an ill-advised detour en route to deliver...",
+  },
+  {
+    id: Math.random(),
+    rating: 5,
+    name: "Once Upon A Time In The west",
+    date: "1968",
+    type: "Western",
+    description:
+      "A mysterious stranger with a harmonica joins forces with a notorious desperado to protect a beautiful widow from a ruthless assassin working for the railroad.",
+  },
+  {
+    id: Math.random(),
+    rating: 4,
+    name: "Taxi Driver",
+    date: "1976",
+    type: "Crime | Drama",
+    description:
+      "A mentally unstable veteran works as a nighttime taxi driver in New York City, where the perceived decadence and sleaze fuels his urge for violent action by attempting to liberate a presidential campaign worker and an underage prostitute.",
+  },
+  {
+    id: Math.random(),
+    rating: 4,
+    name: "Mad Max",
+    date: "1979",
+    type: "Action | Adventure",
+    description:
+      "In a self-destructing world, a vengeful Australian policeman sets out to stop a violent motorcycle gang.",
+  },
+  {
+    id: Math.random(),
+    rating: 3,
+    name: "Little Miss Sunshine",
+    date: "2006",
+    type: "Comedy | Drama",
+    description:
+      "A family determined to get their young daughter into the finals of a beauty pageant take a cross-country trip in their VW bus.",
+  },
+  {
+    id: Math.random(),
+    rating: 5,
+    name: "KILL BILL: VOL. 1",
+    date: "2003",
+    type: "Action",
+    description:
+      "The lead character, called 'The Bride,' was a member of the Deadly Viper Assassination Squad, led by her lover 'Bill.'The Bride' decided to escape her life as a killer.",
+    details: "https://www.imdb.com/title/tt0266697/plotsummary?ref_=tt_stry_pl",
+  },
+];
